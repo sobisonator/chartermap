5a3d42b27348ae264bc1d280790d4820335321cf
Sobisonator <bomchasew@gmail.com>
2025-06-02 22:50:11 +0100

    WiP finetuning test using Google gemini and dating clause sample data

--- TextToDB/script/markup_llm.py
+++ TextToDB/script/markup_llm.py
@@ -1,4 +1,6 @@
 from google import genai
+from google.genai.types import HttpOptions, CreateTuningJobConfig
+import time
 import pandas
 
 ABOVE_PROJECT_PATH = "../../../" # TODO: Use globals for these
@@ -9,14 +11,15 @@ GEMINI_API_KEY = open(GEMINI_API_KEY_PATH).readline()
 MARKUP_TYPES_PATH = "../data/markup_types.csv"
 
 LLM_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={GEMINI_API_KEY}"
-LLM_MODEL = "gemma-3-12b-it"
-
-# TODO: Finetuning Gemma on cloud GPU https://huggingface.co/blog/gemma-peft
+LLM_MODEL = "gemma-3-12b-it" # TBD gemini-2.0.flash-lite might be used for finetuning
 
 class MarkupFlagger():
     # Uses Google AI Studio's Gemma 3 12B (free model, text )
     def __init__(self):
-        self.client = genai.Client(api_key = GEMINI_API_KEY)
+        self.client = genai.Client(
+            api_key = GEMINI_API_KEY,
+            http_options = HttpOptions(api_version="v1")    
+        )
         # We define a structure to create a "system-prompt" equivalent
         # Following https://ai.google.dev/gemma/docs/core/prompt-structure
         # Gemma is instruction tuned and so accepts a system prompt
@@ -27,15 +30,91 @@ class MarkupFlagger():
 
         <start_of_turn>user
         """ # Input proceeds here and is capped by end_prompt
+        # ^ May be obsolete
+        # TODO: Review above in light of using fine tuned model
         self.end_prompt = "\\n<end_of_turn>\\n<start_of_turn>model\\n"
 
         markup_types_file = open(MARKUP_TYPES_PATH)
         self.markup_types = pandas.read_csv(markup_types_file, sep="|")
 
-    def get_response(self, prompt):
+        self.generate_training_dataset()
+    
+    def generate_training_dataset(self):
+        # Transform input CSV into a dict with model and user roles
+        # see https://cloud.google.com/vertex-ai/generative-ai/docs/models/tune_gemini/text_tune
+        # In practice, the LLM should return a classification like this:
+        # <type>typename</type><class>classname</class>
+        # In future we can train this to use the IDs for the type and class
+        # ...but for now we will use names
+        self.training_dataset = {
+            "systemInstruction": {
+                "role": "system",
+                "parts": [
+                    {
+                        "text": "All model role text must consist only of strings in the following format "
+                        "<type>typename</type><class>classname</class>"
+                    }
+                ]
+            },
+            "contents": []
+        }
+        for index, row in self.markup_types.iterrows():
+            training_sample = [
+                {
+                    "role": "user",
+                    "parts": [
+                        {
+                            "text": row["Object"], # Input sample
+                        }
+                    ],
+                },
+                {
+                    "role": "model",
+                    "parts": [
+                        {
+                            "text": f"<type>{row["Type"]}</type><class>{row["Class"]}</class>"
+                        }
+                    ]
+                }
+            ]
+            for message in training_sample:
+                self.training_dataset["contents"].append(message)
+
+    def tune_markup_model(self):
+        # https://cloud.google.com/vertex-ai/generative-ai/docs/models/gemini-use-supervised-tuning#google-gen-ai-sdk
+        tuning_job = self.client.tunings.tune(
+            base_model = "gemini-2.0-flash-lite-001",
+            training_dataset = self.training_dataset,
+            config = CreateTuningJobConfig(
+                tuned_model_display_name = "Charter markup identifier test"
+            )
+        )
+        running_states = set([
+            "JOB_STATE_PENDING",
+            "JOB_STATE_RUNNING"
+        ])
+        while tuning_job.state in running_states:
+            print(tuning_job.state)
+            tuning_job = self.client.tunings.get(name=tuning_job.name)
+            time.sleep(60)
+        print(tuning_job.tuned_model.model)
+        print(tuning_job.tuned_model.endpoint)
+        print(tuning_job.experiment)
+        # Test tuned model
+        response = self.get_response(
+            model = tuning_job.tuned_model.endpoint,
+            prompt = "anno secundo regni nostri, indictione secunda, sub die kalendarum Martis"
+        )
+        print(response)
+
+        if tuning_job.tuned_model.checkpoints:
+            for i, checkpoint in enumerate(tuning_job.tuned_model.checkpoints):
+                print(f"Checkpoints {i+1}: {checkpoint}")
+
+    def get_response(self, model, prompt):
         compiled_prompt = self.system_prompt + prompt + self.end_prompt
         response = self.client.models.generate_content(
-            model=LLM_MODEL,
+            model=model,
             contents = compiled_prompt
         )
         return response.text
@@ -67,3 +146,9 @@ class MarkupFlagger():
                     contents = class_example_string
                 )
         print(self.get_response(class_example_string))
+
+# TESTING
+if True:
+    test = MarkupFlagger()
+    test.generate_training_dataset()
+    test.tune_markup_model()
