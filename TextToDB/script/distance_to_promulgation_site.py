c6531537554d8e83c4cd518b6df85b4969eec036
Sobisonator <bomchasew@gmail.com>
2025-07-06 21:30:26 +0100

    Add new data

---
+++ TextToDB/script/distance_to_promulgation_site.py
@@ -0,0 +1,17 @@
+from geopy import distance
+import pandas as pd
+
+GEODATA = "../data/All estates main map.csv"
+
+df = pd.read_csv(GEODATA,sep=",")
+
+for index, row in df.iterrows():
+    estate_location = (row["Estate Lat Fallback"],row["Estate Lon Fallback"])
+    promulgation_location = (row["Prom Lat"], row["Prom Lon"])
+    try:
+        promulgation_estate_distance = distance.geodesic(estate_location, promulgation_location).km
+        df.at[index, "Distance"] = promulgation_estate_distance
+    except Exception as e:
+        print(e)
+
+df.to_csv(GEODATA, index=False)
