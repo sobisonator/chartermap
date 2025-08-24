f1737094e149fb12445e1b6d2ab106a5125065fb
Sobisonator <battenberger1919@gmail.com>
2025-08-24 10:00:24 +0100

    WIP create witness CSV

--- TextToDB/script/main.py
+++ TextToDB/script/main.py
@@ -2,13 +2,6 @@ from src import *
 
 if True:
     # TODO: Make this a class defined in witness_search
-    witness_tree = ET.parse(WITNESS_PROCESSED_XML_PATH)
-    witness_root = witness_tree.getroot()
-    for charter in witness_root:
-        print(charter.tag, charter.attrib)
-        for witness_match in charter:
-            for element in witness_match:
-                print(element.tag, element.text)
-                # TODO: Save each witness_match into a CSV for alignment with the master spreadsheet
-
+    witness_tree = WitnessData(WITNESS_PROCESSED_XML_PATH)
+    witness_tree.create_witness_csv()
     # TODO: Lookup and save the witness ID from PASE
--- TextToDB/script/witness_search.py
+++ TextToDB/script/witness_search.py
@@ -6,6 +6,24 @@ sawyer_number = "S1507"
 
 results = search(f"site:https://pase.ac.uk/pase/?list=person {witness_name} {sawyer_number}",advanced=True)
 
+class WitnessData():
+    def __init__(self, witness_xml):
+        witness_tree = ET.parse(witness_xml)
+        self.witness_root = witness_tree.getroot()
+    
+    def create_witness_csv(self):
+        for charter in self.witness_root:
+            sawyer_id = (charter.attrib["sawyer_id"])
+            table = {}
+            i = 1
+            for witness_match in charter:
+                fields = []
+                for field in witness_match:
+                    fields.append(field.text)
+                fields.append(sawyer_id)
+                print(fields)
+                table[i] = fields
+
 # Testing
 if False:
     first_result = next(results)
