1002752de9541a3c0936dcd03ec69da38bac9bf4
Sobisonator <battenberger1919@gmail.com>
2025-09-04 21:55:06 +0100

    XML and CSV files with witnesses

--- TextToDB/script/main.py
+++ TextToDB/script/main.py
@@ -1,5 +1,26 @@
 from src import *
 
+if False:
+    def fix_tag_content(xml_text):
+        pattern = re.compile(r"<([A-Za-z0-9_]+)>(.*?)</\1>", re.DOTALL)
+
+        def replacer(match):
+            tag = match.group(1)
+            content = match.group(2).strip()
+            fixed_content = content.replace("<","&lt;").replace(">","&gt;")
+            return f"<{tag}>{fixed_content}</{tag}>"
+        
+        return pattern.sub(replacer, xml_text)
+
+    with open(WITNESS_PROCESSED_XML_PATH) as f:
+        data = f.readlines()
+        with open(f"{WITNESS_PROCESSED_XML_PATH}_fixed", "a") as newfile:
+            for line in data:
+                fixed_xml = fix_tag_content(line)
+                newfile.write(fixed_xml)
+        
+
+
 if True:
     witness_tree = WitnessData(WITNESS_PROCESSED_XML_PATH)
     witness_tree.create_witness_csv(WITNESS_PROCESSED_CSV_PATH)
